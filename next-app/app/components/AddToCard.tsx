4cfff40056b9a118a9fcb239fd29a4fa9145bd55
khanhvo-amblique <khanh.vo@amblique.com>
2024-08-28 18:37:52 +0700

    1.7 Render client only button component in server side using lib => good

---
+++ next-app/app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={() => console.log('click')}>Add to Card</button>
+  )
+}
+
+export default AddToCard
--- next-app/app/components/ProductCard.tsx
+++ next-app/app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
-'use client';
 import React from 'react'
+import AddToCard from './AddToCard';
 
 const ProductCard = () => {
   return (
     <div>
-        <button onClick={() => console.log('click')}>Add to Card</button>
+        <AddToCard />
     </div>
   )
 }
