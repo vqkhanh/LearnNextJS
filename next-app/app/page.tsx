9e7b2a31e6d6d0efd4bb12c2be122e8c230dd482
khanhvo-amblique <khanh.vo@amblique.com>
2024-08-28 16:47:38 +0700

    1.6 Use Link component for navigation

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <main>
     <h1>test 1</h1>
-    <a href='/users' >Users</a>
+    <Link href='/users' >Users</Link>
     </main>
   )
  
