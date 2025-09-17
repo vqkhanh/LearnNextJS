75c1af29b44b86ccf12f1baeedaa3d29a2cb135b
khanhvo <khanh.vo@amblique.com>
2025-09-17 23:16:57 +0700

    5.1 Implement get sort order from parameter

--- next-app/app/users/UserTable.tsx
+++ next-app/app/users/UserTable.tsx
@@ -1,36 +1,33 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const UserTable = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'});
-    const users: User[] = await res.json();
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
   return (
-    <table className='table table-bordered'>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            {users.map(user => 
-                <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                </tr>
-            )
-
-            }
-        </tbody>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>
+            <Link href="users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="users?sortOrder=email">Email</Link>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-    // <>
-    // <h1>Users</h1>
-    // <h1>test git vscode</h1>
-    // {new Date().toLocaleTimeString()}
-    // <ul>
-    //   {users.map(user => <li key={user.id}>{user.name}</li>)}
-    // </ul>
-    // </>
-  )
-}
+  );
+};
 
-export default UserTable
+export default UserTable;
