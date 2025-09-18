41d2ac90590b4e5b3b3a205c094b252cc084b931
khanhvo <khanh.vo@amblique.com>
2025-09-18 23:17:53 +0700

    4.11: add error app level page

--- next-app/app/users/UserTable.tsx
+++ next-app/app/users/UserTable.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const UserTable = async ({ sortOrder }: Props) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/xusers", {
     cache: "no-store",
   });
   const users: User[] = await res.json();
