341992635f892f1dea3ec5ddccb84a47ed7972e8
khanhvo <khanh.vo@amblique.com>
2025-09-19 02:01:24 +0700

    5.5: Updating a object

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -12,3 +12,15 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "khanh" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "name is request" }, { status: 400 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: "john" });
+}
