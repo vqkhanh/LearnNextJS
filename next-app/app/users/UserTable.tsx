603b5e8f3d23e7056760508080afa8dab4070178
khanhvo <khanh.vo@amblique.com>
2025-09-18 23:37:43 +0700

    restore prepair error

--- next-app/app/users/UserTable.tsx
+++ next-app/app/users/UserTable.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const UserTable = async ({ sortOrder }: Props) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/xusers", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
   });
   const users: User[] = await res.json();
