13b1438863a0b26be26676023ae43d8be8b797af
khanhvo <khanh.vo@amblique.com>
2025-09-19 01:27:23 +0700

    5.4: Creating a object

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,5 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json("hello");
+export function GET() {
+  return NextResponse.json([
+    { id: 1, name: "khanh" },
+    { id: 2, name: "john" },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) return NextResponse.json({ error: "Name is required" });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
