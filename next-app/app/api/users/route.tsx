e3db8cc7f5a2b70b32c34b2e472d27f442c9b676
khanhvo <khanh.vo@amblique.com>
2025-09-19 22:46:19 +0700

    6.8: Creating data

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 import prisma from "@/prisma/client";
+import { error } from "console";
 
 export async function GET() {
   const users = await prisma.user.findMany();
@@ -12,5 +13,17 @@ export async function POST(request: NextRequest) {
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.issues, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+  if (user)
+    return NextResponse.json({ error: "User already exist" }, { status: 400 });
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(newUser, { status: 201 });
 }
--- next-app/app/api/users/schema.ts
+++ next-app/app/api/users/schema.ts
@@ -2,6 +2,7 @@ import { email, z } from "zod";
 
 const schema = z.object({
   name: z.string().min(3),
+  email: z.string().email(),
 });
 
 export default schema;
