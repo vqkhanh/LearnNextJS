516fd96efc221d94273461c3c7fede2535de14e5
khanhvo <khanh.vo@amblique.com>
2025-09-19 20:40:02 +0700

    6.6: Create a Prisma Client

---
+++ next-app/prisma/client.ts
@@ -0,0 +1,20 @@
+// import { PrismaClient } from "@prisma/client/extension";
+// import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
