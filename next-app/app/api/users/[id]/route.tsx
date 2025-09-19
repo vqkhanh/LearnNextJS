e56eda7cbcf4b3c4b339aad97a4bf956639a2744
khanhvo <khanh.vo@amblique.com>
2025-09-19 21:20:05 +0700

    6.7: Getting Data (one)

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -1,17 +1,21 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   // fecch data from db
   // if not found, return 404 error
   // else return data
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: "khanh" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(
