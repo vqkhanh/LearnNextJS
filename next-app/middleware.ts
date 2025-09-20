c9618f8be3b41a980ebea82a3b7ff6f80a2927d0
khanhvo <khanh.vo@amblique.com>
2025-09-20 18:59:01 +0700

    8.8: Protecting Routes: optimate syntax

--- next-app/middleware.ts
+++ next-app/middleware.ts
@@ -1,7 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import middleware from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default middleware;
 export const config = {
   // *: zero or more
   // +: one or more
