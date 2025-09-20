b9d09689ec18dfce180546da3b90563e2da4cd4f
khanhvo <khanh.vo@amblique.com>
2025-09-21 02:05:25 +0700

    9.3: Creating email template

---
+++ next-app/emails/WelcomeTemplate.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://codewithmosh.com">wwww.codewithmosh.com</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
