b72b7018841ef296dc58f70d122cea7d4749ccfc
khanhvo <khanh.vo@amblique.com>
2025-09-20 15:02:53 +0700

    8.3: Configure Google Provider

--- next-app/app/api/auth/[...nextauth]/route.ts
+++ next-app/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
+import { Secret } from "./../../../../node_modules/next-auth/src/jwt/types";
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({});
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRECT!,
+    }),
+  ],
+});
 
 export { handler as GET, handler as POST };
