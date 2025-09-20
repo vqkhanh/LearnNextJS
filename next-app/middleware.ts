dec864f3639d6e0018fdbd04749c38aa192ce8be
khanhvo <khanh.vo@amblique.com>
2025-09-20 18:54:10 +0700

    8.8: Protecting Routes: understanding middleware work

---
+++ next-app/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/new-page", request.url));
+}
+
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or one
+  matcher: ["/users/:id*"],
+};
