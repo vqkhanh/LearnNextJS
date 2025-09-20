e2b5499a94731d6c2ced7bbbdd583fe9eb6cd144
khanhvo <khanh.vo@amblique.com>
2025-09-20 18:57:06 +0700

    8.8: Protecting Routes: using of next-auth

--- next-app/middleware.ts
+++ next-app/middleware.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import middleware from "next-auth/middleware";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/new-page", request.url));
-}
-
+export default middleware;
 export const config = {
   // *: zero or more
   // +: one or more
