78c0691757b1be8bf747914c7dd66fb8f7be5c6b
khanhvo <khanh.vo@amblique.com>
2025-09-20 16:35:47 +0700

    8.5 Access session token in client

--- next-app/app/NavBar.tsx
+++ next-app/app/NavBar.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
+  if (status == "loading") return null;
   return (
     <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
-      <Link href="/api/auth/signin">Login</Link>
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
