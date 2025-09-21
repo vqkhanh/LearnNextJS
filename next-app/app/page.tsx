c2ac4682b6c2a619d28e2594bb24856389840ce3
khanhvo <khanh.vo@amblique.com>
2025-09-21 08:55:26 +0700

    10.2: Optimizing Images 1

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import student from "@/public/images/student.jpg";
+
 export default async function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Image src={student} alt="student" />
+    </main>
+  );
 }
