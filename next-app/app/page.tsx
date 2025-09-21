f873d759aeabc6e21d71fe8e121cfe8dec07e351
khanhvo <khanh.vo@amblique.com>
2025-09-21 09:30:29 +0700

    10.2: custom fit img

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
         src="https://camo.githubusercontent.com/220922c58a5dc816ea4b4684f19236eb15b86c75f5bc7806a14c85eacd8db4b3/68747470733a2f2f692e706f7374696d672e63632f3051343833394b4e2f4865616c74682d506c75732d496d616765312e706e67"
         alt="student"
         fill
+        style={{ objectFit: "cover" }}
       />
     </main>
   );
