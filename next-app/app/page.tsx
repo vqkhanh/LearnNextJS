86c57e7c563ce954b3e0fc869b60dd63e18757cf
khanhvo <khanh.vo@amblique.com>
2025-09-21 09:45:50 +0700

    10.2: custom img

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -3,13 +3,15 @@ import student from "@/public/images/student.jpg";
 
 export default async function Home() {
   return (
-    <main>
+    <main className="relative h-screen">
       <Image
         src="https://camo.githubusercontent.com/220922c58a5dc816ea4b4684f19236eb15b86c75f5bc7806a14c85eacd8db4b3/68747470733a2f2f692e706f7374696d672e63632f3051343833394b4e2f4865616c74682d506c75732d496d616765312e706e67"
         alt="student"
         fill
         className="object-cover"
-        sizes="100vw"
+        sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
+        quality={100}
+        priority
       />
     </main>
   );
