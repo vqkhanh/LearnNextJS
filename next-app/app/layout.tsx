f2303f554cee6eab38b3c03c7832527979cebefd
khanhvo <khanh.vo@amblique.com>
2025-09-21 12:03:31 +0700

    10.4: Using font nad custom font

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,12 +1,22 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
 import Script from "next/script";
 import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+const poppins = localFont({
+  src: "../public/fonts/poppins-regular-webfont.woff2",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="cyberpunk">
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <AuthProvider>
           <NavBar />
           <main className="p-5">{children}</main>
